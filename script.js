c0d68fc1bf5bbb03511a36f65ff74dae6c135b53
Rostyslav-Buleziuk <125821260+Rostyslav-Buleziuk@users.noreply.github.com>
2023-02-21 23:05:59 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+let input = document.querySelector('input');
+let reversed='';
+function tstjs() {
+    for (let ch of input) {
+        reversed=ch+reversed;
+    }
+    document.getElementById("output").append();
+}
+
