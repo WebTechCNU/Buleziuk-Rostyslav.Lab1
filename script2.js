8a604e2c79159d2544338caf097cbf5a28f7b3ae
Rostyslav-Buleziuk <125821260+Rostyslav-Buleziuk@users.noreply.github.com>
2023-02-23 19:00:05 +0200

    Create script2.js

---
+++ script2.js
@@ -0,0 +1,14 @@
+function numbers() {
+    let input = document.querySelector('#input').value;
+    let numwithlett = '';
+    for (let i = input.length-1;i=0;) {
+        numwithlett+=console.log('input'+'гривня');
+        if (i=2,3,4) {
+            numwithlett+=console.log('input'+'гривні');
+        }
+        if (i=5,6,7,8,9,0) {
+            numwithlett+=console.log('input'+'гривень')
+        }
+    }
+    document.querySelector("#res").innerHTML = numwithlett;
+}
