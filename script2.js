3444e50fcc7fdbc130fa155e805110afd217a483
Rostyslav-Buleziuk <125821260+Rostyslav-Buleziuk@users.noreply.github.com>
2023-02-28 21:07:01 +0200

    Create script2.js

---
+++ script2.js
@@ -0,0 +1,13 @@
+function numbers() { let input = Number(document.querySelector('#input').value);
+let numwithlett;
+if(isNaN(input)){
+    numwithlett = "This is not a number";
+} else if ((input % 100 > 10 && input % 100 < 20) || [0,5,6,7,8,9].includes(input % 10) ){
+    numwithlett = "гривень";
+} else if (input % 10 == 1 ){
+    numwithlett = "гривня";
+} else {
+    numwithlett = "гривні";
+}
+document.querySelector("#res").innerHTML = numwithlett;
+}
