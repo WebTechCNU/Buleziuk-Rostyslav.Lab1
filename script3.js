53b70d9c21cf448e6d642cac9f33f2b1eaa91698
Rostyslav-Buleziuk <125821260+Rostyslav-Buleziuk@users.noreply.github.com>
2023-03-06 17:57:13 +0200

    Delete script3.js

--- script3.js
+++
@@ -1,8 +0,0 @@
-function spacebar() {
-    let input = document.querySelector("#input").value;
-    let res= '';
-    input.replace(/^ +| +$|( ) +/g,"$1")
-    res+=input;
-
-document.querySelector("#res").innerHTML = res;
-}
